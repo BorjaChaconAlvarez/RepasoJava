384e1512d00217549591a431064dad44ee243506
Borja <borjachacon12@gmail.com>
2025-08-01 15:23:16 +0200

    RepasoJava For/While/Do y Matrices

--- Repaso/src/Clasificacion.java
+++ Repaso/src/Clasificacion.java
@@ -4,19 +4,18 @@ public class Clasificacion {
     public static void main(String[] args) {
         System.out.println("CUANTAS CLASIFICACIONES TIENES: ");
         Scanner sc = new Scanner(System.in);
-        double numero = Integer.parseInt(sc.nextLine());
+        var numero = Integer.parseInt(sc.nextLine());
+        var clasificacion = new int[numero];
         double total = 0;
 
-        for (int i = 0 ; i < numero ; i++){
-            System.out.println("Clasificacion " + "[" + i + "]" );
-            int clasificacion = sc.nextInt();
-
-            total = total + clasificacion;
-
+        for (int i = 0; i < numero; i++) {
+            System.out.println("Clasificacion " + "[" + i + "] : ");
+            clasificacion[i] = Integer.parseInt(sc.nextLine());
+            total = total + clasificacion[i];
 
 
         }
-        double media = (total/numero);
-        System.out.println("Media " +media);
+        double media = (total / numero);
+        System.out.println("Media " + media);
     }
 }
